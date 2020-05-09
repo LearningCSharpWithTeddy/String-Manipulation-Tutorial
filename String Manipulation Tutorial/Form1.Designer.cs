b890343d4cc9e957785a26f2613663a491b57a61
LearningCSharpWithTeddy <65052012+LearningCSharpWithTeddy@users.noreply.github.com>
2020-05-08 20:49:05 -0400

    Add files via upload

---
+++ String Manipulation Tutorial/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace String_Manipulation_Tutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSubstring = new System.Windows.Forms.TextBox();
+            this.txtReplaceWith = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Substring";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "String:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Remove";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Replace";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(67, 10);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(291, 20);
+            this.txtString.TabIndex = 4;
+            this.txtString.Text = "The quick brown fox jumps over the lazy dog\'s back";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(175, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Replace With:";
+            // 
+            // txtSubstring
+            // 
+            this.txtSubstring.Location = new System.Drawing.Point(67, 37);
+            this.txtSubstring.Name = "txtSubstring";
+            this.txtSubstring.Size = new System.Drawing.Size(102, 20);
+            this.txtSubstring.TabIndex = 6;
+            // 
+            // txtReplaceWith
+            // 
+            this.txtReplaceWith.Location = new System.Drawing.Point(256, 37);
+            this.txtReplaceWith.Name = "txtReplaceWith";
+            this.txtReplaceWith.Size = new System.Drawing.Size(102, 20);
+            this.txtReplaceWith.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 97);
+            this.Controls.Add(this.txtReplaceWith);
+            this.Controls.Add(this.txtSubstring);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "String Manipulation Tutorial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSubstring;
+        private System.Windows.Forms.TextBox txtReplaceWith;
+    }
+}
+
---
+++ String Manipulation Tutorial/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace String_Manipulation_Tutorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Remove Button
+        {
+            string strSubString = txtSubstring.Text;// The Substring Textbox
+            string strString = txtString.Text;//the quick brown Fox Textbox
+            int SubString_Length = strSubString.Length;//The Length It Pulls Out Of The Substring Textbox
+            int NS_INdex = strString.IndexOf(strSubString);
+            if(SubString_Length == 0)
+            {
+                MessageBox.Show("You Need To Type In A Sub-String!");
+
+            }
+            else if (NS_INdex == -1)
+            {
+                MessageBox.Show("SubString Not Found In String!");
+            }
+            else
+            {
+                txtSubstring.Text = strString.Remove(NS_INdex, SubString_Length);
+                txtSubstring.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strSubString = txtSubstring.Text;
+            string strReplaceString = txtReplaceWith.Text;
+            string strString = txtString.Text;
+            int nsslength = strSubString.Length;
+            int nsrlength = strReplaceString.Length;
+            int nsindex = strString.IndexOf(strSubString);
+            if(nsslength == 0)
+            {
+                MessageBox.Show("You Need To Type In A Sub-String!");
+            }
+            else if (nsindex == -1)
+            {
+                MessageBox.Show("SubString Not Found In String!");
+            }
+            else if (nsrlength == 0)
+            {
+                MessageBox.Show("You Need To Type In a Replace String!");
+            }
+            else
+            {
+                txtString.Text = strString.Replace(strSubString, strReplaceString);
+                txtSubstring.Clear();
+                txtReplaceWith.Clear();
+            }
+
+
+
+        }
+    }
+}
